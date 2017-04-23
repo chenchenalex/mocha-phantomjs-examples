7e83a9e77e5d067ecfef91e0ea4f7a66bbdb5d2e
Alex Chen <n.a.chen@accenture.com>
2017-04-23 19:44:20 +1000

    fix travis build error and update README

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
-const gulp = require('gulp'),
-      mocha = require('gulp-mocha'),
-      mochaPhantomJS = require('gulp-mocha-phantomjs');
+var gulp = require('gulp'),
+    mocha = require('gulp-mocha'),
+    mochaPhantomJS = require('gulp-mocha-phantomjs');
 
 // test single js file
-gulp.task('mocha', () => {
+gulp.task('mocha', function() {
     return gulp.src('test/test.script.js')
-        .pipe(mocha({reporter: 'nyan'}))
+        .pipe(mocha({ reporter: 'nyan' }))
 });
 
 // test js files which manipulates the DOM
-gulp.task('mocha-dom', () => {
+
+gulp.task('mocha-dom', function() {
     return gulp.src('./index.html')
-        .pipe(mochaPhantomJS({reporter: 'nyan'}))
+        .pipe(mochaPhantomJS({ reporter: 'nyan' }))
 });
 
 
